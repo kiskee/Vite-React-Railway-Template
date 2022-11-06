c184b47644f2418b83a1e1f18b6d5a2fb30a4186
Daniel Medina <53805976+kiskee@users.noreply.github.com>
2022-11-05 22:31:49 -0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,56 @@
-import { useState } from 'react'
-import logo from './logo.svg'
-import './App.css'
+import { useState, useEffect } from "react";
+import axios from "axios";
+import Login from "./components/Login";
+import Weekdays from "./components/Weekdays"
+import Test from "./components/Test";
+import {motion} from 'framer-motion'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const urlWithProxy = "/api/obtenerusuarios";
+
+  const [displayLogin, setdisplayLogin] = useState("block");
+  const [displayDays, setDisplayDays] = useState("none");
+
+  useEffect(() => {
+    if (window.sessionStorage.getItem("Logged")) {
+      setdisplayLogin("none");
+      setDisplayDays("block");
+    } else {
+      console.log("No hay sesion");
+    }
+  }, []);
+
+  //console.log(displayLogin);
+
+
+  /*
+  function getDataFromServer() {
+    axios
+      .get(urlWithProxy)
+      .then((res) => console.log(res.data))
+      .catch((err) => {
+        console.error(err);
+      });
+  }
+*/
 
   return (
-    <div className="App">
-      <h1>Hello Word</h1>
+    <div>
+    
+      {/* ---------------------------------- LOGIN --------------------------------- */}
+      <div style={{ display: displayLogin }} className="login">
+        <Login />
+      </div>
+      {/* ---------------------------- Days of the week ---------------------------- */}
+      <motion.div className="daysOfWeek" style={{ display: displayDays }}
+      initial={{ opacity: 0, scale: 0.5 }}
+      animate={{ opacity: 1, scale: 1 }}
+      transition={{ duration: 0.5 }}
+      >
+        <Weekdays/>
+      </motion.div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
